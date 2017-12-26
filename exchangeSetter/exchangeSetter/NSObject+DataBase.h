81ff8d45f9c94383c03348258f27b4dd96f3ae8f
wangkun <wangkun@wangkundeMacBook-Pro.local>
2017-12-26 10:07:16 +0800

    完善setter嵌入写入功能(实际写入根据使用的数据库框架决定)

--- exchangeSetter/exchangeSetter/NSObject+DataBase.h
+++ exchangeSetter/exchangeSetter/NSObject+DataBase.h
@@ -16,20 +16,21 @@
 } \
 
 
-
-
-//新建表
-//删除表
-
 @interface NSObject (DataBase)
 @property (nonatomic, assign) NSTimeInterval  saveiIntervaltime;
 @property (nonatomic, strong, readonly) NSMutableDictionary * saveKeyValue;
 
-
 + (void)wk_exchangeSetter;
-//数据库主key
+//数据库主key  主键不能被忽略
 + (NSArray *)DBMainKey;
+//数据库需要忽略存储的属性(不写入表) 主键不能被忽略
++ (NSArray *)ignoreProperty;
 
-- (void)saveToLocal;//未实现 根据具体需求实现 更新表
+//主键是否可为空  特殊情形，一个模型对应多种情况，每一种情况对应主键不同，但是写表需要全部写入，因为在一个模型里，设计问题
+//默认NO
++ (BOOL)DBMainKeyCanBeEmpty;
+
+//调用FMDB 可重写
+- (void)saveToLocal;
 
 @end
--- exchangeSetter/exchangeSetter/NSObject+DataBase.m
+++ exchangeSetter/exchangeSetter/NSObject+DataBase.m
@@ -14,10 +14,8 @@ static void *WKDataBaseDealloKey;
 
 static WKClassPropertyModel * getPropertyModel(id self, SEL _cmd)
 {
-    NSArray <WKClassPropertyModel *> * arr = [WKClassManager getClassPropertysWithClass:[self class]];
-    
+    NSArray <WKClassPropertyModel *> * arr = [WKClassManager getClassPropertysWithClass:[self class]];    
     NSString * setter = NSStringFromSelector(_cmd);
-    
     WKClassPropertyModel * selectedModel = nil;
     for (WKClassPropertyModel * model in arr) {
         if([model.setterName isEqualToString:setter])
@@ -64,15 +62,13 @@ static Ivar getIvar(id self, SEL _cmd)
     }
 }
 
-
-
 static void new_setter_object(id self, SEL _cmd, id newValue)
 {
     Ivar member = getIvar(self, _cmd);
     //变量存在则赋值
     if (member != NULL) {
         object_setIvar(self, member, newValue);
-        NSLog(@"修改成功");
+//        NSLog(@"修改成功");
         WKClassPropertyModel * model = getPropertyModel(self, _cmd);
         void (*func)(id, SEL,id) = (void *)model.oldsetterIMP;
         func(self,_cmd,newValue);
@@ -90,7 +86,7 @@ static void new_setter_long(id self, SEL _cmd, long long newValue)
     //变量存在则赋值
     if (member != NULL) {
         object_setIvar(self,member,(__bridge id)((void*)newValue));
-        NSLog(@"修改成功");
+//        NSLog(@"修改成功");
         WKClassPropertyModel * model = getPropertyModel(self, _cmd);
         void (*func)(id, SEL,long long) = (void *)model.oldsetterIMP;
         func(self,_cmd,newValue);
@@ -106,8 +102,21 @@ static void new_setter_long(id self, SEL _cmd, long long newValue)
 
 + (void)wk_exchangeSetter
 {
-    NSArray <WKClassPropertyModel *> * arr = [WKClassManager getClassPropertysWithClass:[self class]];
+    //删除需要被忽略的行数，不参与写入功能
+    NSArray * ignorePropertys = [self ignoreProperty];
+    NSMutableArray <WKClassPropertyModel *> * arr = [NSMutableArray arrayWithArray:[WKClassManager getClassPropertysWithClass:[self class]]];
     NSArray <WKClassMethodModel *> * methods = [WKClassManager getClassMethodsWithClass:[self class]];
+    NSMutableArray * ignorePropertyModel = [NSMutableArray array];
+    for (NSString * key in ignorePropertys) {
+        for (WKClassPropertyModel * model in arr) {
+            if ([key isEqualToString:model.name]) {
+                [ignorePropertyModel addObject:model];
+            }
+        }
+    }
+    [arr removeObjectsInArray:ignorePropertyModel];
+    
+    
     for (WKClassMethodModel * methodModel in methods) {
         for (WKClassPropertyModel * model in arr) {
             if([model.setterName isEqualToString:methodModel.name])
@@ -134,7 +143,6 @@ static void new_setter_long(id self, SEL _cmd, long long newValue)
             __unsafe_unretained typeof(self) weakSelf = self; // NOTE: need to be __unsafe_unretained because __weak var will be reset to nil in dealloc
             id deallocHelper = [self addDeallocBlock:^{
                 //移除线程操作
-                
                 [NSObject cancelPreviousPerformRequestsWithTarget:weakSelf selector:@selector(saveToLocal) object:nil];
                 //存储到本地
                 [weakSelf saveToLocal];
@@ -176,8 +184,7 @@ static void new_setter_long(id self, SEL _cmd, long long newValue)
     if (!mainKeyValue) {
         return;
     }
-    
-    
+    //判断数据是否存在
     NSString * existStr = [NSString stringWithFormat:@"select * from %@ where ",NSStringFromClass([self class])];
     NSArray * mainKeys = [mainKeyValue allKeys];
     for (NSUInteger i = 0; i < mainKeys.count ; i ++) {
@@ -185,25 +192,28 @@ static void new_setter_long(id self, SEL _cmd, long long newValue)
         existStr = [existStr stringByAppendingString: i < (mainKeys.count - 1) ? @"and " : @";"];
     }
     
-    NSLog(@"%@",existStr);
-    
-    //更新
-    NSString * updateStr = [NSString stringWithFormat:@"update ZYTaskModel set "];
-    NSArray * saveKeys = [self.saveKeyValue allKeys];
-    for (NSUInteger i = 0; i < saveKeys.count ; i ++) {
-        updateStr = [updateStr stringByAppendingString:[NSString stringWithFormat:@"%@ = %@ ",saveKeys[i],[self.saveKeyValue objectForKey:saveKeys[i]]]];
-        updateStr = [updateStr stringByAppendingString: i < (saveKeys.count - 1) ? @"," : @" where "];
-    }
-    for (NSUInteger i = 0; i < mainKeys.count ; i ++) {
-        updateStr = [updateStr stringByAppendingString:[NSString stringWithFormat:@"%@ = %@ ",mainKeys[i],[mainKeyValue objectForKey:mainKeys[i]]]];
-        updateStr = [updateStr stringByAppendingString: i < (mainKeys.count - 1) ? @"and " : @";"];
-    }
-    NSLog(@"%@",updateStr);
-
-    //需要存储的时候，判断表存不存在，不存在则创建，存在则取数据更新
-    NSLog(@"存储位置");
-    
-    NSLog(@"%@",self.saveKeyValue);
+//    BOOL isExist = [[DataBaseManager sharedManager] isExist:existStr];
+//    //存在则更新
+//    if (isExist) {
+//        //更新
+//        //生成sql语句
+//        NSString * updateStr = [NSString stringWithFormat:@"update ZYTaskModel set "];
+//        NSArray * saveKeys = [self.saveKeyValue allKeys];
+//        for (NSUInteger i = 0; i < saveKeys.count ; i ++) {
+//            updateStr = [updateStr stringByAppendingString:[NSString stringWithFormat:@"%@ = %@ ",saveKeys[i],[self.saveKeyValue objectForKey:saveKeys[i]]]];
+//            updateStr = [updateStr stringByAppendingString: i < (saveKeys.count - 1) ? @"," : @" where "];
+//        }
+//        for (NSUInteger i = 0; i < mainKeys.count ; i ++) {
+//            updateStr = [updateStr stringByAppendingString:[NSString stringWithFormat:@"%@ = %@ ",mainKeys[i],[mainKeyValue objectForKey:mainKeys[i]]]];
+//            updateStr = [updateStr stringByAppendingString: i < (mainKeys.count - 1) ? @"and " : @";"];
+//        }
+//        [[DataBaseManager sharedManager]executeUpdateSql:updateStr];
+//    }
+//    else
+//    {
+//        //不存在直接调用插入(会新建表)
+//        [[DataBaseManager sharedManager] insertTableWithObject:self ignoreColumns:[[self class] ignoreProperty]];
+//    }
     //存储完后 移除
     [self.saveKeyValue removeAllObjects];
 }
@@ -230,9 +240,20 @@ static void new_setter_long(id self, SEL _cmd, long long newValue)
                     case WKPropertyType_Object:
                     {
                         id value = ((id (*)(id, SEL))(void *) objc_msgSend)((id)self, NSSelectorFromString (model.getterName));
-                        NSAssert(value, @"主键不能为空");
-                        [dict setObject:value forKey:pn];
-                        
+                        //主键不能为nil 直接断言
+                        if (![[self class] DBMainKeyCanBeEmpty]) {
+                            NSAssert(value, @"主键不能为空");
+                        }
+                        else //可以则警告
+                        {
+                            if (value != nil) {
+                                [dict setObject:value forKey:pn];
+                            }
+                            else
+                            {
+                                NSLog(@"Waring Waring Waring 主键为nil");
+                            }
+                        }
                     }
                         break;
                     case WKPropertyType_CNumber:
@@ -245,7 +266,6 @@ static void new_setter_long(id self, SEL _cmd, long long newValue)
                         NSAssert(NO, @"主键类型不正确");
                         break;
                 }
-                
             }
         }
     }
@@ -255,6 +275,15 @@ static void new_setter_long(id self, SEL _cmd, long long newValue)
 + (NSArray *)DBMainKey
 {
     return nil;
+}
+
++ (NSArray *)ignoreProperty
+{
+    return nil;
+}
++ (BOOL)DBMainKeyCanBeEmpty
+{
+    return NO;
 }
 
 @end
